218e615a134e2fcb2e11461a68c1bb4c859c493f
Beibarys <beibarys1927@gmail.com>
2021-04-27 22:32:35 +0600

    Alysa

---
+++ Alysa.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+typealias Meter = Int
+
+let lenght: Meter = 50
+
+let lenght1 = 20
+
+let sum: Meter = lenght + lenght1
+
+typealias DoubleInteger = (Int, Int)
+let someConstant: DoubleInteger = (1,2)
+
+typealias DictionaryType = [String:Int]
+var dictionary: DictionaryType = [:]
+dictionary["Apartment123"] = 123
+
+dictionary
